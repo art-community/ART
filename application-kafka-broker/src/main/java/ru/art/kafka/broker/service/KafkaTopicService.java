b8abb8efcf42a09a43a83af3a0ddbaca8c89edca
Padmelina <Padmelina93@gmail.com>
2020-01-21 19:21:11 +0300

    add service class

---
+++ application-kafka-broker/src/main/java/ru/art/kafka/broker/service/KafkaTopicService.java
@@ -0,0 +1,19 @@
+package ru.art.kafka.broker.service;
+
+import ru.art.kafka.broker.configuration.KafkaTopicConfiguration;
+
+public interface KafkaTopicService {
+    static void addTopic(KafkaTopicConfiguration topic) {
+
+    }
+
+    // topic + num of partitions
+    static void addPartitions() {
+
+    }
+
+    // topic
+    static void deleteTopic() {
+
+    }
+}
